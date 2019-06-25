0a6de5f1e4003db020573f2635d7d228bf2653b0
aleksashk <40260846+aleksashk@users.noreply.github.com>
2019-06-25 17:03:28 +0300

    Create task1.java

---
+++ 1_Basics_of_software_code_development/task1.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class task1 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter number a, b, c");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double a = Integer.parseInt(reader.readLine());
+        double b = Integer.parseInt(reader.readLine());
+        double c = Integer.parseInt(reader.readLine());
+        System.out.println(((a - 3)*b/2) + c);
+    }
+}
