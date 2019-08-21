34fbaddcb41d6a829d96f3e99acde081321dd93b
aleksashk <40260846+aleksashk@users.noreply.github.com>
2019-08-21 14:51:17 +0200

    Update Task3.java

--- 2_Algorithmization/Task3.java
+++ 2_Algorithmization/Task3.java
@@ -6,25 +6,26 @@ import java.io.InputStreamReader;
 
 public class Task3 {
     public static void main(String[] args) throws IOException {
-        int size, minNumber, maxNumber;
+        int size;
+        double minNumber, maxNumber;
         int counterPositive = 0;
         int counterNegative = 0;
         int counterZero = 0;
         System.out.print("Введите размерность числовой массива (целое положительное число): ");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         size = Integer.parseInt(reader.readLine());
-        int[]aray = new int[size];
+        double[]aray = new double[size];
         //Для формирования массива произвольными в том числе отрицательными и нулевыми элементами, необходимо задать minNumber. minNumber - это число позволит сместить диапазон с максимальным числом maxNumber в отрицательную плоскость
         System.out.print("Введите число, от которого будет начинаться выборка произвольных чисел для заполнения массива (положительное): ");
-        minNumber = Integer.parseInt(reader.readLine());
+        minNumber = Double.parseDouble(reader.readLine());
         System.out.print("Введите число (верхний предел), до которого будет производиться выборка произвольных чисел для заполнения массива (положительное): ");
-        maxNumber = Integer.parseInt(reader.readLine());
+        maxNumber = Double.parseDouble(reader.readLine());
 
         //заполнение массива произвольными числами
         for (int i = 0; i < aray.length; i++) {
-            aray[i] = (int)(Math.random() * maxNumber) - minNumber;
+            aray[i] = (Math.random() * maxNumber) - minNumber;
         }
-        for (int item: aray) {
+        for (double item: aray) {
             if (item > 0){
                 counterPositive ++;
             }
@@ -36,9 +37,9 @@ public class Task3 {
             }
         }
         //вывод итогового массива
-        System.out.print("aray = [");
-        for (int item : aray) {
-            System.out.print(item + ", ");
+        System.out.println("aray = [");
+        for (double item : aray) {
+            System.out.println("\t\t" + item + ", ");
         }
         System.out.println("\b\b]");
         System.out.print("В данном массиве: " + counterPositive + " положительных элементов " + counterNegative + " отрицательных элементов " + counterZero + " нулевых элементов");
