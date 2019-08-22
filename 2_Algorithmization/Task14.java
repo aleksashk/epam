34fee31e1e164820f8dd88cb33db5027b33b68e6
aleksashk <40260846+aleksashk@users.noreply.github.com>
2019-08-22 10:04:58 +0200

    Update Task14.java

--- 2_Algorithmization/Task14.java
+++ 2_Algorithmization/Task14.java
@@ -9,7 +9,7 @@ import java.io.InputStreamReader;
 
 public class Task14 {
     public static void main(String[] args) throws IOException {
-        System.out.print("Задайте a: ");
+        System.out.print("Задайте a (размер квадратной матрицы): ");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
         int[][]aray = new int[a][a];
