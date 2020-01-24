08a89b87e79fadcca9bceb6397c23435d27e9ae9
aleksashk <40260846+aleksashk@users.noreply.github.com>
2020-01-24 11:05:33 +0200

    Update Task26Main.java

--- 2_Algorithmization/Task26Main.java
+++ 2_Algorithmization/Task26Main.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         int num;
@@ -35,31 +33,32 @@ public class Main {
                 num = DataOption.setNumber();
             }
             num += 2;
-            int[][] aray = new int[num][num];
+            int[][] temp = new int[num][num];
+
             for (int i = 1; i < num - 1; i++) {
                 for (int j = 1; j < num - 1; j++) {
-                    aray[i][j] = 0;
+                    temp[i][j] = 0;
                 }
             }
 
             for (int i = 0, j = 0; j < num; j++) {
-                aray[i][j] = -1;
+                temp[i][j] = -1;
             }
             for (int i = 0, j = num - 1; i < num; i++) {
-                aray[i][j] = -1;
+                temp[i][j] = -1;
             }
             for (int i = num - 1, j = 0; j < num; j++) {
-                aray[i][j] = -1;
+                temp[i][j] = -1;
             }
             for (int i = 0, j = 0; i < num; i++) {
-                aray[i][j] = -1;
+                temp[i][j] = -1;
             }
 
             x = 1;
             y = num / 2;
             a = x;
             b = y;
-            aray[a][b] = d;
+            temp[a][b] = d;
 
             while (d < Math.pow(num - 2, 2)) {//str 26
                 ++d;
@@ -68,59 +67,65 @@ public class Main {
                 xDown = DataOption.getXDown(a);
                 xDownDiagonal = DataOption.getXDownDiagonal(num);
                 yLeftDiagonal = DataOption.getYLeftDiagonal();
-                if (aray[xDown][y] == 0) {
+                if (temp[xDown][y] == 0) {
                     a = xDown;
                     b = y;
                 }
-                if (aray[xUp][yDiagonal] == 0) {
+                if (temp[xUp][yDiagonal] == 0) {
                     a = xUp;
                     b = yDiagonal;
                 }
-                if (aray[xUp][yDiagonal] == -1) {
-                    if (aray[xUp][y] == -1 && aray[xDownDiagonal][yDiagonal] == 0) {
+                if (temp[xUp][yDiagonal] == -1) {
+                    if (temp[xUp][y] == -1 && temp[xDownDiagonal][yDiagonal] == 0) {
                         a = xDownDiagonal;
                         b = yDiagonal;
-                    } else if (aray[xUp][y] != -1) {
+                    } else if (temp[xUp][y] != -1) {
                         a = xUp;
                         b = yLeftDiagonal;
                     }
                 }
 
-                aray[a][b] = d;
+                temp[a][b] = d;
                 y = b;
             }
-            for (int i = 1; i < num - 1; i++) {
-                for (int j = 1; j < num - 1; j++) {
+            int[][] aray = new int[num - 2][num - 2];
+            for (int i = 0, m = 1; i < num - 2; i++, m++) {
+                for (int j = 0, n = 1; j < num - 2; j++, n++) {
+                    aray[i][j] = temp[m][n];
+                }
+            }
+            num -= 2;
+            for (int i = 0; i < num; i++) {
+                for (int j = 0; j < num; j++) {
                     System.out.printf("%6d", aray[i][j]);
                 }
                 System.out.println();
 
             }
-            for (int i = 1; i < num - 1; i++) {
+            for (int i = 0; i < num; i++) {
                 int sum = 0;
-                for (int j = 1; j < num - 1; j++) {
+                for (int j = 0; j < num; j++) {
                     sum += aray[i][j];
                 }
                 System.out.println("sum items of aray in " + i + " row is " + sum + ";");
             }
-            for (int j = 1; j < num - 1; j++) {
+            for (int j = 0; j < num; j++) {
                 int sum = 0;
-                for (int i = 1; i < num - 1; i++) {
+                for (int i = 0; i < num; i++) {
                     sum += aray[i][j];
                 }
                 System.out.println("sum items of aray in " + j + " column is " + sum + ";");
             }
             int s1 = 0;
-            for (int i = num - 2, j = 1; i >= 1; i--, j++) {
+            for (int i = num - 1, j = 0; i >= 0; i--, j++) {
                 s1 += aray[i][j];
             }
             System.out.println("sum items of aray in first diagonal is " + s1 + ";");
             int s2 = 0;
-            for (int i = 1, j = i; i < num - 1; i++) {
+            for (int i = 0, j = i; i < num; i++) {
                 s2 += aray[i][j];
             }
             System.out.println("sum items of aray in second diagonal is " + s2 + ";");
-
         }
         if (chose == 2) {
             msg = DataOption.MAIN_MSG_SECOND;
