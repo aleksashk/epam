48bf71a35cfcf614a5858b6567af1f8f724892c5
aleksashk <40260846+aleksashk@users.noreply.github.com>
2020-01-24 20:08:31 +0200

    Create 9.java

---
+++ 2_Algorithmization/arrays of arrays/9.java
@@ -0,0 +1,54 @@
+//Задана матрица неотрицательных чисел. Посчитать сумму элементов в каждом столбце. Определить, какой столбец содержит максимальную сумму.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task19 {
+    public static void main(String[] args) throws IOException {
+        int n, m, index;
+        double maxSum;
+
+        System.out.print("Задайте m (количество строк матрицы): ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        m = Integer.parseInt(reader.readLine());
+        System.out.print("Задайте n (количество столбцов матрицы): ");
+        n = Integer.parseInt(reader.readLine());
+        //объявляем массив, в которых будут записываться значения сумм столбцов матрицы aray
+        double[] sumAray =new double[n];
+        double sum = 0;
+        double[][] aray = new double[m][n];
+
+        //заполняем матрицу произвольными числами
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                aray[i][j] = Math.random() * m * n;
+            }
+        }
+        System.out.println("Исходная матрица: aray[" + m + "][" + n + "] = ");
+        for (double[] es : aray) {
+            for (double e : es) {
+                System.out.print(e + "; ");
+            }
+            System.out.println();
+        }
+        //подсчет суммы элементов в каждом столбце исходной матрицы
+        for (int i = 0; i < n; i++) {
+            sum = 0;
+            for (int j = 0; j < m; j++) {
+                sumAray[i] += aray[j][i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println("сумма элементов " + i + " столбца матрицы aray составила: " + sumAray[i] + ";");
+        }
+        maxSum = sumAray[0];
+        index = 0;
+        for (int i = 0; i < sumAray.length; i++) {
+            if(sumAray[i] > maxSum){
+                maxSum = sumAray[i];
+                index = i;
+            }
+        }
+        System.out.println("Максимальную сумму элементов содержит " + index + " столбец матрицы aray. Сумма элементов в столбце составила: " + maxSum + ";");
+    }
+}
