063d0066741d4b475bfc6f672a9bb8b8fbd6906e
aleksashk <40260846+aleksashk@users.noreply.github.com>
2020-02-18 15:16:02 +0200

    Update 1.java

--- 3_Strings_and_basics_of_text_processing/work with a string as with a String or StringBuilder object/1.java
+++ 3_Strings_and_basics_of_text_processing/work with a string as with a String or StringBuilder object/1.java
@@ -1,1 +1,75 @@
-/**************/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String msg = "Input string with a lot of spaces: ";
+        print(msg);
+
+        msg = initMsg();
+        char[] array = msg.toCharArray();
+
+        int newLength = findSize(array);
+        int[] newArray = new int[newLength];
+
+        findNumberSpaces(array, newArray);
+
+        int max = findMax(newArray);
+
+        msg = "Max number of spaces in the string is: " + max + ";";
+        print(msg);
+    }
+
+    public static String initMsg() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static void print(String msg) {
+        System.out.print(msg);
+    }
+
+    public static int findSize(char[] array) {
+        int counter = 0;
+        for (int i = 0; i < array.length; ) {
+            if (Character.isSpaceChar(array[i])) {
+                counter++;
+                {
+                    while (Character.isSpaceChar(array[i])) {
+                        if (++i == array.length) {
+                            break;
+                        }
+                    }
+                }
+            } else {
+                i++;
+            }
+        }
+        return counter;
+    }
+
+    public static void findNumberSpaces(char[] array, int[] newArray) {
+        for (int i = 0, j = 0; i < array.length; ) {
+            if (Character.isSpaceChar(array[i])) {
+                while (Character.isSpaceChar(array[i])) {
+                    newArray[j]++;
+                    if (++i == array.length) {
+                        break;
+                    }
+                }
+                j++;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    public static int findMax(int[] array) {
+        int max = array[0];
+        for (int element : array) {
+            if (element > max) {
+                max = element;
+            }
+        }
+        return max;
+    }
+}
